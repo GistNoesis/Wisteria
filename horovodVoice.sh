fc4f57dedeebb6c4f96a6bd46b7042245c84e401
unrealwill <gistnoesis@gmail.com>
2019-03-06 18:26:29 +0100

    adding horovod launching script

---
+++ horovodVoice.sh
@@ -0,0 +1,6 @@
+mpirun -np 2 \
+    -H localhost:2 \
+    -bind-to none -map-by slot \
+    -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
+    -mca pml ob1 -mca btl ^openib \
+    python3 voiceTranscriptor.py
